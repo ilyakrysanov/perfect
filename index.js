200e059680c4b2b4951248090cf72f0816d80a44
ilyakrysanov <138809467+ilyakrysanov@users.noreply.github.com>
2023-07-06 20:11:08 +0300

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = '«Каждый человек нуждается в чем-то исключительном в эпоху, когда за деньги можно иметь все». © Райнхольд Месснер'; 
+});
